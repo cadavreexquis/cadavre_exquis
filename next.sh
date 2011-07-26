c618f774413f1b452db3912ecd0f087dd9d66252
lmazardo <luc@mazardo.com>
2011-07-26 02:05:14 +0200

    initial revision

---
+++ next.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+
+coders=" cbegot@gmail.com ggdx@laposte.net etienne.charignon@gmail.com pascal@grange.nom.fr luc@mazardo.com"
+
+i=0
+for coder in ${coders}
+do
+   if [ ${i} -eq 0 ]; 
+   then
+     echo "next is ${coder}"
+     first=${coder}
+     i=$((${i} + 1))
+   else
+     new_order="${new_order} ${coder}"
+   fi
+done
+new_order="${new_order} $first"
+echo ${new_order}
+
+sed -i "s/${coders}/${new_order}/g" $0 
