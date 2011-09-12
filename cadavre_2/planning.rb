cf5df1ef0df8539bea6865075ee5d64eaa611d7f
cbegot <cbegot@gmail.com>
2011-09-12 19:46:54 +0300

    Edited cadavre_2/planning.rb via GitHub

--- cadavre_2/planning.rb
+++ cadavre_2/planning.rb
@@ -4,11 +4,14 @@ require "test/unit"
 
 class Generator
    def initialize(value)
-     @next= value.keys.join
+   
+    if value.respond_to?("keys")
+	 @next= value.keys.join 
+	end
    end
    def next
-     @next
-    end
+    @next
+   end
 end
 
 class GeneratorTest < Test::Unit::TestCase
@@ -16,5 +19,10 @@ class GeneratorTest < Test::Unit::TestCase
   def test_with_hash_parameter
 	assert_equal("ac", Generator.new({ "a" => 1, "c" => 1 }).next )
   end
+    
+  def test_with_string_parameter
+	assert_equal(nil, Generator.new("abc").next )
+  end
+
 end
 
