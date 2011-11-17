0cfcd2e7a806ad864c8d10e62d2854f0ce3318ca
pgrange <pascal@grange.nom.fr>
2011-11-17 22:17:33 +0100

    FIX

--- cadavre_1/setenv.sh
+++ cadavre_1/setenv.sh
@@ -1,8 +1,8 @@
 gem_binaries=$(gem environment | grep "EXECUTABLE DIRECTORY:" | sed 's,.*: \(.*\),\1,g')
 
-printf "Modify your PATH as following for having gem binaries in PATH\n"
-printf "\n"
+printf "#Modify your PATH as following for having gem binaries in PATH\n"
+printf "#\n"
 printf "\texport PATH=$PATH:${gem_binaries}\n"
-printf "\n"
-printf "or eval me\n"
-printf '\teval $(./setenv.sh)'
+printf "#\n"
+printf "#or eval me\n"
+printf '#\teval $(./setenv.sh)'
